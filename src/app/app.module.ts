9735f60c644e71a257c84714b12817f75eb27c33
basilkot <basilkot@gmail.com>
2021-11-17 22:06:07 +0200

    base layout template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { ToolbarPanelComponent } from './components/toolbar-panel/toolbar-panel.component';
+import { PreviewComponent } from './components/preview/preview.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    ToolbarPanelComponent,
+    PreviewComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
