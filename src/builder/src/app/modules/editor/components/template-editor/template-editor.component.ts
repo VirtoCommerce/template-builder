9bd8a60318e4168d77907f5bb6059cca2ea2eb7c
basilkot <basilkot@gmail.com>
2022-01-17 19:28:24 +0200

    add dnd functionality

--- src/builder/src/app/modules/editor/components/template-editor/template-editor.component.ts
+++ src/builder/src/app/modules/editor/components/template-editor/template-editor.component.ts
@@ -4,6 +4,8 @@ import { ChangeDetectionStrategy, Component, EventEmitter, HostBinding, Input, O
 import { SectionsSchemasList, TemplateModel } from '@editor/models';
 import { SectionModel } from '@shared/models';
 
+import { helpers } from '@editor/services';
+
 @Component({
     selector: 'app-template-editor',
     templateUrl: './template-editor.component.html',
@@ -44,12 +46,13 @@ export class TemplateEditorComponent implements OnInit {
         return !!this.openedItems[sectionId];
     }
 
-    onOpenChanged(sectionId: string, isOpened: boolean) {
+    onOpenChanged(event: MouseEvent, sectionId: string, isOpened: boolean) {
+        event.stopPropagation();
         this.openedItems[sectionId] = isOpened;
     }
 
-    onVisibleChanged(value: { sectionIndex: number, blockIndex: number | null, value: boolean }) {
-        this.itemVisibleChanged.emit(value);
+    onVisibleChanged(value: boolean, sectionIndex: number, blockIndex: number | null) {
+        this.itemVisibleChanged.emit({ sectionIndex, blockIndex, value });
     }
 
     reorderSections(event: CdkDragSortEvent<SectionModel>) {
@@ -64,10 +67,53 @@ export class TemplateEditorComponent implements OnInit {
         console.log('drag released', event);
     }
 
+    blockDragStarted() {
+        console.log('block drag started');
+    }
+
+    blockDragCompleted() {
+        console.log('block drag released');
+    }
+
     getTemplateName(): string {
         if (this.template && this.template.settings) {
             return <string>this.template.settings['name'];
         }
         return '[no name]';
     }
+
+    getSectionIcon(section: SectionModel): string | null {
+        if (this.sectionsSchemas && this.sectionsSchemas[section.type]) {
+            return this.sectionsSchemas[section.type].icon;
+        }
+        return null; // todo: add default section icon
+    }
+
+    getBlockIcon(block: SectionModel): string | null {
+        if (this.blocksSchemas && this.blocksSchemas[block.type]) {
+            return this.blocksSchemas[block.type].icon;
+        }
+        return null; // todo: add default block icon
+    }
+
+    getSectionName(section: SectionModel): string {
+        return this.getItemName(section, this.sectionsSchemas);
+    }
+
+    getBlockName(block: SectionModel): string {
+        return this.getItemName(block, this.blocksSchemas)
+            || this.getItemName(block, this.sectionsSchemas);
+    }
+
+    private getItemName(item: SectionModel, schemas: SectionsSchemasList | null): string {
+        if (schemas) {
+            return helpers.getSectionName(item, schemas);
+        }
+        return '[no name]';
+    }
+
+    hasChildren(section: SectionModel): boolean {
+        return !!this.sectionsSchemas && this.sectionsSchemas[section.type] && !!this.sectionsSchemas[section.type]?.blocks?.length;
+    }
+
 }
