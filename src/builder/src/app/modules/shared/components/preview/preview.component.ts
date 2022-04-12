d0a0addeb4eab099650700f6580a7e456afda0ab
basilkot <basilkot@gmail.com>
2022-04-12 10:12:22 +0200

    add "previewframe" for preview component

--- src/builder/src/app/modules/shared/components/preview/preview.component.ts
+++ src/builder/src/app/modules/shared/components/preview/preview.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-preview',
-  templateUrl: './preview.component.html',
-  styleUrls: ['./preview.component.scss']
+    selector: 'app-preview',
+    templateUrl: './preview.component.html',
+    styleUrls: ['./preview.component.scss']
 })
 export class PreviewComponent implements OnInit {
 
-  constructor() { }
+    @Input() previewMode: boolean = true;
+    @Input() presetName?: string;
 
-  ngOnInit(): void {
-  }
+    constructor() { }
+
+    ngOnInit(): void {
+    }
 
 }
