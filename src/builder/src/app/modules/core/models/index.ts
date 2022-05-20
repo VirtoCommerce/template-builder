86d14f9d38ead15436089b3a8520bc25a9b99d8a
basilkot <basilkot@gmail.com>
2022-05-20 17:21:00 +0200

    fix mistake in file name

--- src/builder/src/app/modules/core/models/index.ts
+++ src/builder/src/app/modules/core/models/index.ts
@@ -1,7 +1,7 @@
 export * from './section.model';
 export * from './section.schema';
 export * from './control.context';
-export * from './model-cahnged.event-args';
+export * from './model-changed.event-args';
 
 export * from './components';
 export * from './controls';
