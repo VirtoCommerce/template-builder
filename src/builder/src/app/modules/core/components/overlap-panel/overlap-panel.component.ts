deb51d6006276eb62a2de5c557b7c443c4977029
basilkot <basilkot@gmail.com>
2022-06-29 13:53:42 +0200

    set panel expander behaviour

--- src/builder/src/app/modules/core/components/overlap-panel/overlap-panel.component.ts
+++ src/builder/src/app/modules/core/components/overlap-panel/overlap-panel.component.ts
@@ -1,5 +1,7 @@
+import { AfterContentInit, AfterViewInit, ChangeDetectorRef, HostBinding, HostListener } from '@angular/core';
 import { EnvironmentRef } from '@integration/services';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
+import { Observable, of, delay } from 'rxjs';
 
 @Component({
     selector: 'app-overlap-panel',
@@ -10,17 +12,61 @@ export class OverlapPanelComponent implements OnInit {
 
     @Input() expandable = true;
 
+    @HostListener('window:resize', ['$event'])
+    onResize() {
+        this.changeWidth();
+    }
+
+    private _interval: any;
+
+    contentWidth: number | null = null;
+    expanderPosition: number | null = null;
     isOpened = false; // todo: maybe should be stored in state or in url
 
-    constructor(private windowRef: EnvironmentRef) {}
+    constructor(
+        private windowRef: EnvironmentRef,
+        private cdr: ChangeDetectorRef,
+        private elementRef: ElementRef) { }
 
     ngOnInit(): void { }
 
-    getContentWidth(): number | null {
-        if (this.isOpened) {
-            return this.windowRef.nativeWindow.innerWidth / 2;
-        }
-        return null;
+    ngAfterViewInit(): void {
+        this._interval = setInterval(() => {
+            this.changeWidth();
+        }, 1000);
     }
+
+    ngOnDestroy(): void {
+        clearInterval(this._interval);
+    }
+
+    toggle() {
+        this.isOpened = !this.isOpened;
+        this.changeWidth();
+    }
+
+    changeWidth() {
+        setTimeout(() => {
+            if (this.isOpened) {
+                this.contentWidth = this.windowRef.nativeWindow.innerWidth / 2;
+            } else {
+                this.contentWidth = null;
+            }
+            this.expanderPosition = this.contentWidth || this.elementRef.nativeElement.offsetWidth;
+            this.cdr.detectChanges();
+        });
+    }
+
+    // getContentWidth(): number | null {
+    //     if (this.isOpened) {
+    //         return this.windowRef.nativeWindow.innerWidth / 2;
+    //     }
+    //     return null;
+    // }
+
+    // getPanelWidth(): number {
+    //     this.cdr.detectChanges();
+    //     return this.getContentWidth() || this.elementRef.nativeElement.offsetWidth;
+    // }
 
 }
