9d5e8db69e13b37c419d62980550dfeadd43e5d8
basilkot <basilkot@gmail.com>
2022-07-14 13:18:54 +0200

    fixes in files control

--- src/builder/src/app/modules/core/controls/files/files.component.ts
+++ src/builder/src/app/modules/core/controls/files/files.component.ts
@@ -25,9 +25,10 @@ export class FilesComponent extends BaseControlDirective<FilesDescriptor> {
     private elementSubscription: Subscription | null = null;
 
     elementForm: FormGroup | null = null;
-    control!: FileUploadControl;
+    control!: FileUploadControl; // control used for d-n-d only
     selectedFile: AssetFile | null = null;
     expanded: boolean = false;
+    innerValue: AssetFile[] = [];
 
     constructor(
         private modals: ModalService,
@@ -40,25 +41,25 @@ export class FilesComponent extends BaseControlDirective<FilesDescriptor> {
         super.initContent();
         this.control = this.createUploadControl();
 
-        let init = true; // todo: fix it
-        const value = this.getValue();
-        this.control.setValue(value);
+        this.innerValue = this.getValue();
         this.subscription = this.control.valueChanges.subscribe(items => {
-            if (!init) {
+            if (items && items.length) {
+                const files = items.map((x, index) => this.convertValueToFile(x, index));
                 this.uploadFiles(<any>items);
+                this.innerValue.push(...files);
                 this.raiseValueChanged();
+                this.control.setValue([]); // reset control
             }
-            init = false;
         });
     }
 
     reorderItems(event: CdkDragDrop<any>) {
-        const value = this.control.value;
+        const value = this.innerValue;
         const item = value[event.previousIndex];
         value.splice(event.previousIndex, 1);
         value.splice(event.currentIndex, 0, item);
-        this.control.setValue(value);
-        console.log(event);
+        this.raiseValueChanged();
+        // this.control.setValue(value);
     }
 
     toggleList() {
@@ -85,15 +86,15 @@ export class FilesComponent extends BaseControlDirective<FilesDescriptor> {
         if (!this.descriptor.skipRemoveConfirmation) {
             this.modals.confirm(this.descriptor.removeMessage || 'Do you want to delete this item?').subscribe((data: any) => {
                 if (data) {
-                    this.control.removeFile(file);
+                    this.deleteFileInternal(file);
                 }
             });
         } else {
-            this.control.removeFile(file);
+            this.deleteFileInternal(file);
         }
     }
 
-    uploadItem(file: AssetFile, index: number, items: Array<AssetFile>) {
+    uploadItem(file: AssetFile, index: number) {
         if (!file.uploaded && !file.uploading) {
             file.uploading = true;
             file.error = null;
@@ -120,13 +121,13 @@ export class FilesComponent extends BaseControlDirective<FilesDescriptor> {
     }
 
     getMaxListHeight(): string {
-        return this.control.value.length <= (this.descriptor.collapseThreshold || 6) || this.expanded
+        return this.innerValue.length <= (this.descriptor.collapseThreshold || 6) || this.expanded
             ? 'inherit'
             : 'calc((' + (this.descriptor.collapseCount || 4) + ' + .5) * (.5rem + 20px))'
     }
 
     getContext(item: AssetFile, index: number): ControlContext {
-        const element = this.convertItemToValue(item);
+        const element = this.convertFileToValue(item);
         return {
             ...this.context,
             item: this.controlValue,
@@ -140,6 +141,14 @@ export class FilesComponent extends BaseControlDirective<FilesDescriptor> {
     protected override destroyContent(): void {
         this.unsubscribe();
         this.unsubscribeElement();
+    }
+
+    private deleteFileInternal(file: AssetFile) {
+        if (this.selectedFile === file) {
+            this.selectedFile = null;
+        }
+        this.innerValue.splice(this.innerValue.indexOf(file), 1);
+        this.raiseValueChanged();
     }
 
     private unsubscribe() {
@@ -157,7 +166,7 @@ export class FilesComponent extends BaseControlDirective<FilesDescriptor> {
     }
 
     private raiseValueChanged() {
-        const value = this.convertItemsToValue(<any>this.control.value);
+        const value = this.convertFilesToValue(<any>this.innerValue);
         this.onValueChanged(value);
     }
 
@@ -173,40 +182,47 @@ export class FilesComponent extends BaseControlDirective<FilesDescriptor> {
         if (!Array.isArray(files)) {
             files = [files];
         }
-        const result = files.map((x: any, index: number) => {
-            // here x is a custom model or string
-            const result = (typeof x === 'string')
-                ? <AssetFile>{
-                    lastModified: 0,
-                    name: x.startsWith('data:') ? '[inline data]' : x.substring(x.lastIndexOf('/') + 1),
-                    webkitRelativePath: x,
-                    url: x,
-                    uploaded: true
-                }
-                : <AssetFile>{
-                    lastModified: 0,
-                    name: x[this.descriptor.filenameField || 'filename']
-                        || x[this.descriptor.urlField || 'url'].substring(x[this.descriptor.urlField || 'url'].lastIndexOf('/') + 1),
-                    webkitRelativePath: x[this.descriptor.urlField || 'url'],
-                    data: x,
-                    url: x[this.descriptor.urlField || 'url'],
-                    uploaded: true
-                };
-            const context = this.getContext(result, index);
-            result.previewUrl = this.data.getPreviewUrl(result, this.descriptor, context);
-            return result;
-        });
+        const result = files.map((x: any, index: number) => this.convertValueToFile(x, index));
+        return result;
+    }
+
+    private convertValueToFile(item: any, index: number): AssetFile {
+        // here item is a custom model or string
+        let result: AssetFile;
+        if (typeof item === 'string') {
+            result = <AssetFile>{
+                lastModified: 0,
+                name: item.startsWith('data:') ? '[inline data]' : item.substring(item.lastIndexOf('/') + 1),
+                webkitRelativePath: item,
+                url: item,
+                uploaded: true
+            };
+        } else if (item instanceof File) {
+            result = <AssetFile>item;
+            // here result is a single model item with data property and default File properties
+            if (this.descriptor.element && this.descriptor.element.length) {
+                // need to generate 'data' property
+                result.data = coreHelpers.createDefaultObject(this.descriptor.element);
+            }
+        } else {
+            result = <AssetFile>{
+                lastModified: 0,
+                name: item[this.descriptor.filenameField || 'filename']
+                    || item[this.descriptor.urlField || 'url'].substring(item[this.descriptor.urlField || 'url'].lastIndexOf('/') + 1),
+                webkitRelativePath: item[this.descriptor.urlField || 'url'],
+                data: item,
+                url: item[this.descriptor.urlField || 'url'],
+                uploaded: true
+            };
+        }
+        const context = this.getContext(result, index);
+        result.previewUrl = this.data.getPreviewUrl(result, this.descriptor, context);
         return result;
     }
 
     private uploadFiles(items: Array<AssetFile>) {
         items.forEach((x: AssetFile, index: number) => {
-            // here x is a single model item with data property and default File properties
-            if (this.descriptor.element && this.descriptor.element.length && !x.data) {
-                // need to generate 'data' property
-                x.data = coreHelpers.createDefaultObject(this.descriptor.element);
-            }
-            this.uploadItem(x, index, items);
+            this.uploadItem(x, index);
         });
     }
 
@@ -221,14 +237,14 @@ export class FilesComponent extends BaseControlDirective<FilesDescriptor> {
         });
     }
 
-    private convertItemsToValue(items: AssetFile[]): any {
+    private convertFilesToValue(items: AssetFile[]): any {
 
         if (!items || !items.length) {
             return null;
         }
 
         const singleFile = !this.descriptor.multiple;
-        let result = items.map(x => this.convertItemToValue(x));
+        let result = items.map(x => this.convertFileToValue(x));
 
         if (singleFile) {
             result = result[0];
@@ -237,7 +253,7 @@ export class FilesComponent extends BaseControlDirective<FilesDescriptor> {
         return result;
     }
 
-    private convertItemToValue(item: AssetFile): any {
+    private convertFileToValue(item: AssetFile): any {
         const hasElement = !!this.descriptor.element && this.descriptor.element.length > 0;
         const propertiesSet = this.descriptor.urlField || this.descriptor.filenameField;
 
