094295f83f53c787b710b3ea17d9729cb00545be
Basil Kotov <basilkot@gmail.com>
2024-09-26 13:46:36 +0100

    hide hover message from logging

--- src/builder/src/app/modules/shared/components/live-preview/live-preview.component.ts
+++ src/builder/src/app/modules/shared/components/live-preview/live-preview.component.ts
@@ -79,7 +79,9 @@ export class LivePreviewComponent implements OnInit {
             if (this.frame) {
                 const frame = this.frame.nativeElement as HTMLIFrameElement;
                 const message = { ...msg, source: 'builder' };
-                console.log(message);
+                if (message.type !== 'hover') {
+                    console.log(message);
+                }
                 frame.contentWindow?.postMessage(message, this.url);
             }
         });
